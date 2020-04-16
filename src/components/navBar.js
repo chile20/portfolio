8995ba787a21c88db926bf2f6f112f9b593ac8fc
Chi Le <51723885+chile20@users.noreply.github.com>
2020-04-16 16:51:00 -0700

    Update

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -14,14 +14,14 @@ class Nav extends Component {
             <div class="container max-w-4xl mx-auto flex flex-wrap p-8 flex-col md:flex-row items-center">
               <nav class="md:m-auto flex flex-wrap items-center text-base justify-center">
                 
-                <Link to="/home" className="mr-16 lg:p-4 py-3 px-0 border-transparent border-b-2 hover:border-orange-400">Home</Link>
+                <Link to="/" className="mr-16 lg:p-4 py-3 px-0 border-transparent border-b-2 hover:border-orange-400">Home</Link>
                 <Link to="/work" className="mr-16 lg:p-4 py-3 px-0 border-transparent border-b-2 hover:border-orange-400">Work</Link>
                 <Link to="/about" className="lg:p-4 py-3 px-0 border-transparent border-b-2 hover:border-orange-400">About</Link>
               
               </nav>
             </div>
           </header>
-            <Route exact path="/home" component={Home} />
+            <Route exact path="/" component={Home} />
             <Route path="/work" component={Work} />
             <Route path="/about" component={About} />
   
