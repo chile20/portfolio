73ff3deae75853229c117a3de9afd2cc5e5a8a79
Chi Le <51723885+chile20@users.noreply.github.com>
2020-04-17 13:43:58 -0700

    Remove darkUI from App.js

--- src/App.js
+++ src/App.js
@@ -1,101 +1,30 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
-import { Helmet, HelmetProvider } from "react-helmet-async";
 // Import Browser Router and Pages
 import { BrowserRouter, Switch, Route } from 'react-router-dom'; 
 import Home from './components/Home';
 import Work from './components/Work';
 import About from './components/About';
 //Import Components
-import torchLight from './assets/torch-light.svg';
-import torchDark from './assets/torch-dark.svg'
 
-const useLocalState = (key, defaultValue) => {
-  const [value, setValue] = useState(() => {
-    const storedValue = localStorage.getItem(key);
-    return storedValue === null ? defaultValue : JSON.parse(storedValue);
-  });
-
-  useEffect(() => {
-    const listener = (e) => {
-      if (e.storageArea === localStorage && e.key === key) {
-        setValue(JSON.parse(e.newValue));
-      }
-    };
-    window.addEventListener("storage", listener);
-
-    return () => {
-      window.removeEventListener("storage", listener);
-    };
-  }, [key]);
-
-  const setValueInLocalStorage = (newValue) => {
-    setValue((currentValue) => {
-      const result =
-        typeof newValue === "function" ? newValue(currentValue) : newValue;
-      localStorage.setItem(key, JSON.stringify(result));
-      return result;
-    });
-  };
-
-  return [value, setValueInLocalStorage];
-};
-
-export default function App() {
-  const [theme, setTheme] = useLocalState("theme", "light");
-
+function App() {
   return (
-    <HelmetProvider>
-      <Helmet>
-        <body data-theme={theme} />
-      </Helmet>
-
-      <button
-        className="absolute top-0 right-0 focus:outline-none"
-        onClick={() => setTheme((cur) => (cur === "light" ? "dark" : "light"))}
-      >
-        <img className=" h-12 w-18 p-4"
-          src={theme === "light" ? torchDark : torchLight}
-          alt="toggle theme"
-        />
-      </button>
-      
-        <BrowserRouter basename="/portfolio"> 
-          <div className="App">
-            <Switch>
-                <Route exact path="/">
-                  <Home />
-                </Route>
-                <Route path="/about">
-                  <About />
-                </Route>
-                <Route path="/work">
-                  <Work />
-                </Route>
-            </Switch>
-          </div>
-        </BrowserRouter>
-  </HelmetProvider>
+    <BrowserRouter basename="/portfolio"> 
+    <div className="App">
+      <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/work">
+            <Work />
+          </Route>
+      </Switch>
+    </div>
+    </BrowserRouter>
   );
 }
 
-// function App() {  return (
-//     <BrowserRouter basename="/portfolio"> 
-//       <div className="App">
-//         <Switch>
-//             <Route exact path="/">
-//               <Home />
-//             </Route>
-//             <Route path="/about">
-//               <About />
-//             </Route>
-//             <Route path="/work">
-//               <Work />
-//             </Route>
-//         </Switch>
-//     </div>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
+export default App;
--- src/components/nav.js
+++ src/components/nav.js
@@ -13,7 +13,7 @@ class Nav extends Component {
                 
                 <NavLink exact to="/" activeClassName="active" className="mr-16 lg:p-4 py-3 px-0 border-transparent border-b-2 hover:border-orange-400">Home</NavLink>
                 <NavLink exact to="/work" activeClassName="active" className="mr-16 lg:p-4 py-3 px-0 border-transparent border-b-2 hover:border-orange-400">Work</NavLink>
-                <NavLink exact to="/about" activeClassName="active" className="mr-16 lg:p-4 py-3 border-transparent border-b-2 hover:border-orange-400">About</NavLink>
+                <NavLink exact to="/about" activeClassName="active" className="lg:p-4 py-3 border-transparent border-b-2 hover:border-orange-400">About</NavLink>
                 <DarkModeButton />
               </nav>
             </div>
