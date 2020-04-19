b51f30a8b18eccfd13c606b05b0598eb041c35d7
Chi Le <51723885+chile20@users.noreply.github.com>
2020-04-19 15:53:33 -0700

    Remove Nav from App.js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,22 @@ import React from 'react';
 import './App.css';
 // Import Browser Router and Pages
 import { BrowserRouter, Switch, Route } from 'react-router-dom'; 
-import Home from './containers/Home';
 import Work from './containers/Work';
 import About from './containers/About';
 //Import Components
+import Nav from './components/nav';
 
 function App() {
   return (
     <BrowserRouter basename="/portfolio"> 
     <div className="App">
+      <Nav />
       <Switch>
           <Route exact path="/">
-            <Home />
+            <Work />
           </Route>
           <Route path="/about">
             <About />
-          </Route>
-          <Route path="/work">
-            <Work />
           </Route>
       </Switch>
     </div>
