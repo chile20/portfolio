352ffd798e5de8ceb681c7ff51695e0838d0bf34
Chi Le <51723885+chile20@users.noreply.github.com>
2020-04-20 18:34:27 -0700

    Add link to logo

--- src/components/nav.js
+++ src/components/nav.js
@@ -8,7 +8,7 @@ class Nav extends Component {
     return (
       <header className="body-font">
   <div className="container max-w-4xl mx-auto flex flex-wrap pt-8 flex-col lg:flex-row items-center justify-end">
-    <a className="flex order-first lg:order-none lg:w-1/5 title-font items-center sm:items-center sm:justify-center mb-4 md:mb-0">
+    <a className="flex order-first lg:order-none lg:w-1/5 title-font items-center sm:items-center sm:justify-center mb-4 md:mb-0" href="/">
       <span className=" sm:text-3xl text-2xl logo">CHILE</span>
     </a>
     <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
@@ -17,8 +17,8 @@ class Nav extends Component {
   </div>
               <div className="container max-w-4xl mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center"> 
               <nav className="body-font md:m-auto flex flex-wrap items-center text-base justify-center">  
-                <NavLink exact to="/" activeClassName="active" className="mr-16 lg:p-4 py-3 px-0 border-transparent border-b-2 hover:border-orange-400">Work</NavLink>
-                <NavLink exact to="/about" activeClassName="active" className="lg:p-4 py-3 border-transparent border-b-2 hover:border-orange-400">About</NavLink>
+                <NavLink exact to="/" activeClassName="active" className="focus:outline-none mr-16 lg:p-4 py-3 px-0 border-transparent border-b-2 hover:border-orange-400">Work</NavLink>
+                <NavLink exact to="/about" activeClassName="active" className="focus:outline-none lg:p-4 py-3 border-transparent border-b-2 hover:border-orange-400">About</NavLink>
               </nav>
             </div>
 </header>
