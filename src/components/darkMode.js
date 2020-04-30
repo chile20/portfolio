ad164e7cea77b76c31baa104f42485bfb74138f8
Chi Le <51723885+chile20@users.noreply.github.com>
2020-04-30 00:06:43 -0700

    New Darkmode

---
+++ src/components/darkMode.js
@@ -0,0 +1,28 @@
+import React from "react";
+import useDarkMode from "use-dark-mode";
+
+import Toggle from "react-toggle";
+import { Sun, Moon} from 'react-feather'
+import "../assets/css/custom.css";
+
+const DarkModeToggle = () => {
+const darkMode = useDarkMode(true);
+  
+
+  return (
+    <div className="flex">
+      <Toggle
+        checked={darkMode.value}
+        icons={{
+          checked: (<Moon size={16} fill="white"/>
+          ),
+          unchecked: ( <Sun size={16} fill="white"/>
+          ),
+        }}
+        onChange={darkMode.toggle}
+      />
+    </div>
+  );
+};
+
+export default DarkModeToggle;
