54843c4d79bd6746bf7b09b023650a45e0fbd25c
Chi Le <51723885+chile20@users.noreply.github.com>
2021-02-02 22:23:23 -0800

    Updates

---
+++ precache-manifest.96ecbe223c201902aa7a9a3221417b6f.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4294da6e1d850a25da55edcabdd5ee3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8b4154bf780cbcb28c84",
+    "url": "/portfolio/static/css/main.58fb21c9.chunk.css"
+  },
+  {
+    "revision": "f1cc4a65f2070fa8e552",
+    "url": "/portfolio/static/js/2.5dbdcd24.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.5dbdcd24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b4154bf780cbcb28c84",
+    "url": "/portfolio/static/js/main.af63048f.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "65d1715cef7f093a8817241932e7e78e",
+    "url": "/portfolio/static/media/chile-resume.65d1715c.pdf"
+  },
+  {
+    "revision": "36e10457fd7e7e471f0e461b4fc33fb6",
+    "url": "/portfolio/static/media/img-about-banner.36e10457.png"
+  },
+  {
+    "revision": "328693092c44de7e8c6eaf21d4c3210e",
+    "url": "/portfolio/static/media/img-cover-cosmadetic.32869309.jpg"
+  },
+  {
+    "revision": "1ed49b6a2b696c31c077c6dd6393af87",
+    "url": "/portfolio/static/media/img-cover-doyou.1ed49b6a.png"
+  },
+  {
+    "revision": "64644cfd5ee2863a906b50b641438dbf",
+    "url": "/portfolio/static/media/img-cover-doyou2.64644cfd.png"
+  },
+  {
+    "revision": "4a2eb87401e2a082aef3c3d44d25ad8c",
+    "url": "/portfolio/static/media/img-cover-firetshirt.4a2eb874.png"
+  },
+  {
+    "revision": "5c2db0ac6409b1b5f10780966cf9c29b",
+    "url": "/portfolio/static/media/img-cover-leap.5c2db0ac.png"
+  },
+  {
+    "revision": "193a6ee59f5a2048dcb9d000cd3ae0c9",
+    "url": "/portfolio/static/media/img-cover-leapweb.193a6ee5.png"
+  },
+  {
+    "revision": "b90e2ac03ff340e325b1642eacc8a3cd",
+    "url": "/portfolio/static/media/img-cover-redairport.b90e2ac0.png"
+  },
+  {
+    "revision": "1a5ae9277f5768157405b945f02222cf",
+    "url": "/portfolio/static/media/img-cover-reddispatch.1a5ae927.png"
+  },
+  {
+    "revision": "c41056d3f6b550d634e4de1580d706f2",
+    "url": "/portfolio/static/media/img-cover-redenvelope.c41056d3.png"
+  },
+  {
+    "revision": "b991bc69c9c3db616f1cb6173c21de95",
+    "url": "/portfolio/static/media/img-cover-travaria.b991bc69.png"
+  },
+  {
+    "revision": "a5a00294cb485630bf65aa22f6171ae0",
+    "url": "/portfolio/static/media/img-cover-xlady.a5a00294.jpg"
+  },
+  {
+    "revision": "1092fc0874ba5ed5b5913d6888302612",
+    "url": "/portfolio/static/media/kos-2x.1092fc08.gif"
+  },
+  {
+    "revision": "68ed3533b2bce47653383d0660a856d6",
+    "url": "/portfolio/static/media/starbucks-2x.68ed3533.png"
+  }
+]);
